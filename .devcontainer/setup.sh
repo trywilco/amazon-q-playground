26e1acf3764380f6e1a254ec79bcc00dfae42ea6
Shem <smagnezi8@gmail.com>
2025-05-18 13:58:33 +0000

    install amazon q cli

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+
 WILCO_ID="`cat .wilco`"
 CODESPACE_BACKEND_HOST=$(curl -s "${ENGINE_BASE_URL}/api/v1/codespace/backendHost?codespaceName=${CODESPACE_NAME}&portForwarding=${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" | jq -r '.codespaceBackendHost')
 CODESPACE_BACKEND_URL="https://${CODESPACE_BACKEND_HOST}"
 export ENGINE_EVENT_ENDPOINT="${ENGINE_BASE_URL}/users/${WILCO_ID}/event"
 
-# Update engine that codespace started for user
-curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }"
+# Install Amazon Q CLI
+echo "Installing Amazon Q CLI..."
+export AMAZON_Q_VERSION=latest && \
+    curl --proto '=https' --tlsv1.2 -sSf "https://desktop-release.q.us-east-1.amazonaws.com/${AMAZON_Q_VERSION}/amazon-q.deb" -o amazon-q.deb && \
+    sudo apt-get update && \
+    sudo apt-get install -y ./amazon-q.deb && \
+    rm ./amazon-q.deb && \
+    sudo rm -rf /var/lib/apt/lists/*
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update
-sudo apt-get install -y google-cloud-sdk
+# Update engine that codespace started for user
+curl -L -X POST "${ENGINE_EVENT_ENDPOINT}" -H "Content-Type: application/json" --data-raw "{ \"event\": \"github_codespace_started\" }
